0e31d3f50fc240e63af1f03cb6a233abdecf18ec
Yumeng Sun <yumeng.sun@sap.com>
2021-03-14 15:22:52 -0400

    update db

--- util/db.js
+++ util/db.js
@@ -2,54 +2,51 @@
 const MongoClient = require('mongodb').MongoClient;
 
 const URI = process.env.DB_URL;
-const DB = "qq";
-const COLLECTION = "songs";
+const DB = {
+    qq: 'qq',
+    bili: 'bilibili',
+    netease: 'netease'
+};
+const COLLECTION = {
+    songs: 'songs',
+    videos: 'videos',
+    channel: 'channel',
+    history: 'history',
+    ranks: 'ranks',
+    summary: 'summary',
+    lastUpdate: 'lastUpdate'
+};
 
+//generic
 async function connect ( uri=URI ) {
     const client = new MongoClient(uri, { useUnifiedTopology: true });
     await client.connect();
     return client;
 }
 
-async function _find( client, query, db=DB, collection=COLLECTION ) {
-    return client.db(db)
-    .collection(collection)
-    .findOne(query);
-}
-
-async function findByDate( client, sDate, db=DB, collection=COLLECTION ) {
-    return _find( client, {updatedAt: new RegExp(sDate)}, db, collection);
-}
-
-async function findBiliVideoData( client, sDate ) {
-    return findByDate( client, sDate, 'bilibili', 'videos' );
-}
-
-async function findBiliChannelData( client, sDate ) {
-    return findByDate( client, sDate, 'bilibili', 'channel' );
-}
-
-async function findBiliHistoryData( client, sDate ) {
-    return findByDate( client, sDate, 'bilibili', 'history' );
-}
-
-async function findOneChart( client ) {
-    return _find( client, {tag: 'charts'}, DB, 'ranks' );
-}
-
-async function findAll( client, db=DB, collection=COLLECTION ) {
+async function findAll( client, db=DB.qq, collection=COLLECTION.songs ) {
     return client.db(db)
     .collection(collection)
     .find({}).toArray();
 }
 
-async function insertOne( client, data, db=DB, collection=COLLECTION  ) {
+async function _find( client, query, db=DB.qq, collection=COLLECTION.songs ) {
+    return client.db(db)
+    .collection(collection)
+    .findOne(query);
+}
+
+async function findByDate( client, sDate, db=DB.qq, collection=COLLECTION.songs ) {
+    return _find( client, {updatedAt: new RegExp(sDate)}, db, collection);
+}
+
+async function insertOne( client, data, db=DB.qq, collection=COLLECTION.songs  ) {
     return client.db(db)
     .collection(collection)
     .insertOne( data );
 }
 
-async function upsertOne( client, query, data, db=DB, collection=COLLECTION  ) {
+async function upsertOne( client, query, data, db=DB.qq, collection=COLLECTION.songs  ) {
     return client.db(db)
     .collection(collection)
     .updateOne(
@@ -63,40 +60,65 @@ async function upsertOne( client, query, data, db=DB, collection=COLLECTION  ) {
     );
 }
 
-async function upsertOneByDate( client, sDate, data, db=DB, collection=COLLECTION ) {
-    return upsertOne( client, {updatedAt: new RegExp(sDate)}, data, db, collection );
-}
 
-async function updateYesterdayFavData( client, data ) {
-    return upsertOne( client, {tag: 'yesterday'}, data, DB, 'lastUpdate' );
-}
-
-async function updateBiliHistoryData( client, sDate, data ) {
-    return upsertOneByDate( client, {updatedAt: new RegExp(sDate)}, data, 'bilibili', 'history' );
-}
-
-async function updateSummary( client, data ) {
-    return upsertOne( client, {tag: 'summary'}, data, DB, 'summary' );
-}
-
-async function updateCharts( client, data ) {
-    return upsertOne( client, {tag: 'charts'}, data, DB, 'ranks' );
-}
-
-async function findYesterdayFavData( client ) {
-    return client.db(DB)
-    .collection('lastUpdate')
-    .findOne({
-        tag: 'yesterday'
-    });
-}
-
-async function updateOneById( client, _id, data, db=DB, collection=COLLECTION  ) {
+async function updateOneById( client, _id, data, db=DB.qq, collection=COLLECTION.songs  ) {
     return client.db(db)
     .collection(collection)
     .updateOne( {_id }, {$set: data} );
 }
 
+async function upsertOneByDate( client, sDate, data, db=DB.qq, collection=COLLECTION.songs ) {
+    return upsertOne( client, {updatedAt: new RegExp(sDate)}, data, db, collection );
+}
+
+//qq
+async function updateYesterdayFavData( client, data ) {
+    return upsertOne( client, {tag: 'yesterday'}, data, DB.qq, COLLECTION.lastUpdate );
+}
+
+async function findYesterdayFavData( client ) {
+    return client.db(DB.qq)
+    .collection(COLLECTION.lastUpdate)
+    .findOne({
+        tag: 'yesterday'
+    });
+}
+
+async function updateSummary( client, data ) {
+    return upsertOne( client, {tag: 'summary'}, data, DB.qq, COLLECTION.summary );
+}
+
+async function updateCharts( client, data ) {
+    return upsertOne( client, {tag: 'charts'}, data, DB.qq, COLLECTION.ranks );
+}
+//BILIBILI
+async function findBiliVideoData( client, sDate ) {
+    return findByDate( client, sDate, DB.bili, COLLECTION.videos );
+}
+
+async function findBiliChannelData( client, sDate ) {
+    return findByDate( client, sDate, DB.bili, COLLECTION.channel );
+}
+
+async function findBiliHistoryData( client, sDate ) {
+    return findByDate( client, sDate, DB.bili, COLLECTION.history );
+}
+
+async function findOneChart( client ) {
+    return _find( client, {tag: 'charts'}, DB.qq, COLLECTION.ranks );
+}
+
+async function updateBiliHistoryData( client, sDate, data ) {
+    return upsertOneByDate( client, sDate, data, DB.bili, COLLECTION.history );
+}
+
+async function getNeteaseRanksByDate( client, sDate ) {
+    return findByDate( client, sDate, DB.netease, COLLECTION.ranks );
+}
+
+async function updateNeteaseRanks( client, sDate, data ) {
+    return upsertOneByDate( client, sDate, data, DB.netease, COLLECTION.ranks );
+}
 module.exports = {
     connect,
     findAll,
@@ -113,5 +135,7 @@ module.exports = {
     updateYesterdayFavData,
     findYesterdayFavData,
     updateSummary,
-    updateCharts
+    updateCharts,
+    getNeteaseRanksByDate,
+    updateNeteaseRanks
 };
