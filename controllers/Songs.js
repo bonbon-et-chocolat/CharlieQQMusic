fa4862399a436671c04c691e9cfa08f34f6e1642
Yumeng Sun <yumeng.sun@sap.com>
2021-04-03 00:13:56 -0400

    update data

--- controllers/Songs.js
+++ controllers/Songs.js
@@ -211,8 +211,8 @@ function _getLiveData({ hitSongs, hitInfo, favInfo, weeklyListenCountInfo, updat
         formatted.record = record ? record.data : undefined;
         formatted.score = score;
         if( formatted.mid === '004OQ5Mt0EmEzv' && !score ) {
-            formatted.score = 274054;
-            listenCnt = '40w+';
+            formatted.score = 324008;
+            listenCnt = '50w+';
             formatted.record = [];
         }
         if( formatted.mid === '003akgwo0qN5bE' && !score ) {
