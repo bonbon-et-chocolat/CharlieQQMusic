368afe466ec61013dfae9f98463708be3cce9e5b
Yumeng Sun <yumeng.sun@outlook.com>
2021-06-01 10:23:49 -0400

    add fnote 0

--- controllers/Songs.js
+++ controllers/Songs.js
@@ -85,7 +85,7 @@ async function _getHitSongs({ mid=MID }) {
 
                 return song.songInfo.title.indexOf( '伴奏' )===-1
                 // eslint-disable-next-line max-len
-                && ( song.songInfo.fnote===4001 || song.songInfo.fnote===4009 || song.songInfo.mid === LVREN || song.songInfo.id === JINGYU || song.songInfo.mid === PIAOYANG )
+                && ( song.songInfo.fnote===0 || song.songInfo.fnote===4001 || song.songInfo.fnote===4009 || song.songInfo.mid === LVREN || song.songInfo.id === JINGYU || song.songInfo.mid === PIAOYANG )
                 && song.songInfo.action
                 && song.songInfo.action.msgdown!==3
                 && song.songInfo.album.time_public
